5474a1f581bda85600b2b9bbdeda27ac3871fe36
Egon Willighagen <egon.willighagen@gmail.com>
2019-09-27 20:52:50 +0200

    First bits of input outputted: PMID, DOI, PMCID, article name

--- src/output.js
+++ src/output.js
@@ -1,10 +1,16 @@
 export default {
   quickstatements (csl) {
     var output = ""
-    console.log("foo")
     for(var i = 0; i < csl.length; i++) {
       var item = csl[i];
       output = output + '\tCREATE\n\n\tLAST\tP31\tQ13442814\n';
+      if (item.DOI) output = output + '\tLAST\tP356\t\"' + item.DOI + '\"\n';
+      if (item.title) {
+          output = output + '\tLAST\tP1476\t\"' + item.title + '\"\n';
+          output = output + '\tLAST\tLen\t\"' + item.title + '\"\n';
+      }
+      if (item.PMID) output = output + '\tLAST\tP698\t\"' + item.PMID + '\"\n';
+      if (item.PMCID) output = output + '\tLAST\tP932\t\"' + item.PMCID + '\"\n';
     }
     return output
   }
